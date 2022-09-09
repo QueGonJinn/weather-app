40ca6893cd34f698aa7fa9dd45d75c3336467eeb
QueGonJinn <molialow13@gmail.com>
2022-09-09 08:27:41 +0300

    :feat: location search implementation

---
+++ components/SearchBox.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import cities  from '../lib/city.list.json';
+import Link from 'next/link';
+
+export default function SearchBox() {
+	const [query, setQuery] = React.useState('');
+	const [result, setResult] = React.useState([]);
+
+	const onChange = (elem) => {
+		const {value} = elem.target;
+		setQuery(value);
+
+		let matchingCities = [];		
+		if (value.length > 3) {
+			for (let city of cities) {
+				if (matchingCities.length >= 5) {
+					break;
+				}
+
+				const match = city.name.toLowerCase().startsWith(value.toLowerCase());
+
+				if (match) {
+					matchingCities.push(city);
+				}
+			}
+		}
+		return setResult(matchingCities);
+	};
+
+	return (
+		<div className="search">
+			<h1 className='search__title'>Enter the name of the city</h1>
+			<input type="text" value={query} onChange={onChange}/>
+
+			{query.length > 3 && (
+				<ul>
+					{result.length > 0 ? (
+						result.map((city, index) => (
+							<li key={index}>
+								<Link href='/location/cityname'>
+									<a>
+										{city.name}
+										{city.useState ? `, ${city.state}` : ''}
+										<span>{city.country}</span>
+									</a>
+								</Link>
+							</li>
+						)) 
+					) : (
+						<li className="search__no-results">No results found</li>
+					)}
+				</ul>
+			)}
+		</div>
+  	)
+};
